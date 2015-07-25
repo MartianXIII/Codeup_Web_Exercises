52a4833a34a968e83ac5cc0d8f5a965d0f183803
Mars <marianomarquis@gmail.com>
2015-07-25 10:24:56 -0500

    Hi

--- public/Log.php
+++ public/Log.php
@@ -1,9 +1,17 @@
 <?php
 class Log
 {
+
+    public $handle;// add a new property to this class
     public $filename;
     public $date;
-    public function logMessage($logLevel, $message)
+
+    public function __construct()// open file and wait
+    {
+      //Take in a parameter call $prefix if nothing is passed default to log
+      $prefix->date = $logDateTime;
+    }
+    public function logMessage($logLevel, $message)// add a constructor to my log class
     {
         $logDayTime = date("Y-m-d h:i:s a e");
         $handle = fopen($this->filename, 'a');
@@ -13,17 +21,32 @@ class Log
         } else {
             file_put_contents($this->filename, $stringToWrite);
         }
-        fclose($handle);
+        fclose($handle); // add a destructor to the close $handle
     }
     public function info($message)
     {
         $logLevel = 'INFO';
-        return $this->logMessage('INFO', $message);
+        return $this->logMessage('INFO', $message);//Update $logMessage to use Handle no longer open and closes its own
     }
     public function error($message)
     {
         $logLevel = 'ERROR';
         return $this->logMessage('ERROR', $message);
     }
+
+    public function __destruct()
+    {
+      //close $handle when class is destroyed
+    }
 }
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LOG LOG Log</title>
+  </head>
+  <body>
+    <h1>LOG</h1>
+  </body>
+</html>
