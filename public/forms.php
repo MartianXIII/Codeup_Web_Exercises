2f875c7fffa71312a938c25ccf387a030a58f632
Mars <marianomarquis@gmail.com>
2015-08-05 08:45:43 -0500

    When was this from...?lol

---
+++ public/forms.php
@@ -0,0 +1,12 @@
+<form method='post'>
+  NAME: <input type='text' placeholder="namegame" name='name' id='name' /><br />
+
+  Email: <input type='text' name='email' id='email' /><br />
+
+  Website: <input type='text' name='website' id='website' /><br />
+
+  Comment:<br />
+  <textarea name='comment' id='comment'></textarea><br />
+
+  <input type='submit' value='Submit' />
+</form>
