75846f7e17fb7a717dbc2272ff182ac071635ce9
Mars <marianomarquis@gmail.com>
2015-09-04 09:38:51 -0500

    Navvy.php

---
+++ public/Navvvy.php
@@ -0,0 +1,13 @@
+<nav
+          <!- Collect the nav links, forms, and other content for toggling ->
+          <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            
+              <ul class="nav navbar-nav navbar-right">
+       <li><a href="/ads.create.php">Create Ad</a></li>
+       <li><a href="/users.show.php">Profile</a></li>
+       <li><a href="/auth.logout.php">Logout</a></li>
+
+     </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+</nav>
