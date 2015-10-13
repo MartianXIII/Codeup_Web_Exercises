7f84075c08e2517638d04ed5f5fbee414986673f
Mars <marianomarquis@gmail.com>
2015-10-13 14:50:28 -0500

    added ACME 6 for of iterator and truncate

--- public/Fibonacci.php
+++ public/Fibonacci.php
@@ -16,5 +16,29 @@
         return f;
       };
     </script>
+
+    <script>
+        //ACME6(2015)
+        //Iterator objexts enable custom iteration
+        let fibonacci = {
+          [Symbol.iterator] () {
+            let pre = 0, cur = 1;
+            return {
+              next() {
+                [pre, cur] = [cur, pre + cur];
+                return { done: false, value: cur }
+              }
+            }
+          }
+        }
+
+        for (var n of fibonacci) {
+          //truncate the sequence at 1000
+          if (n > 1000)
+            break;
+          console.log(n);
+        }
+
+    </script>
   </body>
 </html>
