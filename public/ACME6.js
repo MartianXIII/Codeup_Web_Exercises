18c462ed98d7fda2bf9d2b4790cc2a71f97e25e4
Mars <marianomarquis@gmail.com>
2015-10-24 17:15:41 -0500

    Ecma Glossary

--- public/ACME6.js
+++ public/ACME6.js
@@ -1,9 +1,9 @@
 //Constants or Immutable variables
 //varible is Immutable, the assigned content is stil an alterable Object
-//ECMA6
+//ECMA6vv
 const PI = 3.141593;
 PI > 3.0;
-//ECMA 5
+//ECMA 5vv
 Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     value:        3.141593,
     enumerable:   true,
@@ -11,3 +11,44 @@ Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     configurable: false
 })
 PI > 3.0;
+
+
+//Scoping
+//Blocked scoped variables without hoisting
+//ECMA6vv
+for (let i = 0; i < a.length; i++) {
+    let x = a[i];
+    …
+}
+for (let i = 0; i < b.length; i++) {
+    let y = b[i];
+    …
+}
+
+let callbacks = [];
+for (let i = 0; i <= 2; i++) {
+    callbacks[i] = function () { return i * 2; };
+}
+callbacks[0]() === 0;
+callbacks[1]() === 2;
+callbacks[2]() === 4;
+//ECMA5vv
+var i, x, y;
+for (i = 0; i < a.length; i++) {
+    x = a[i];
+    …
+}
+for (i = 0; i < b.length; i++)
+    y = b[i];
+    …
+}
+
+var callbacks = [];
+for (var i = 0; i <= 2; i++) {
+    (function (i) {
+        callbacks[i] = function() { return i * 2; };
+    })(i);
+}
+callbacks[0]() === 0;
+callbacks[1]() === 2;
+callbacks[2]() === 4;
