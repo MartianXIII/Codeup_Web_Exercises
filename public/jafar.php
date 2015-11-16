e60baca9010996d6a75557f69b7c41ef3bfac00e
Mars <marianomarquis@gmail.com>
2015-11-16 13:59:15 -0600

    FIFO

---
+++ public/jafar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<script>
+  
+// ECMA 5
+function (x) {
+  return x + 1;
+}
+
+function (x, y) {
+  return x + y;
+}
+// ECMA 6
+x => x + 1
+(x, y) => x + y
+
+//Lots and Lots of functions today
+
+
+</script>
+</body>
+</html>
