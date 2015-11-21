dcb74a7892f403e0e8f8a2215b7ae84be81a0606
Mars <marianomarquis@gmail.com>
2015-11-21 14:18:33 -0600

    this

--- public/ACME6.js
+++ public/ACME6.js
@@ -162,5 +162,8 @@ var chars = [ ...str ] // ["f", "o", "o"]
 //ACME 5
 var params = ["hello", true, 7];
 var other = [1, 2].concat(params); // [1, 2, "hello", true, 7];
+f.apply(undefined, [1, 2].concat(params)) === 9;
 
+var str = "foo";
+var chars = str.split(""); // ["f", "o", "o"]
 //ACME 6
