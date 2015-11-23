5e29d6fdfdaef7d397babe6cc12147cf1604f4ca
Mars <marianomarquis@gmail.com>
2015-11-23 14:31:54 -0600

    Template Literals

--- public/ACME6.js
+++ public/ACME6.js
@@ -177,3 +177,8 @@ want to buy ${card.amount} ${card.product} for
 a total of ${card.amount * card.unitprice} bucks?`
 
 //ACME5
+var customer = { name: "Foo" };
+var card = { amount: 7, product: "Bar", unitprice: 42 };
+message = "Hello" + customer.name + ",\n" +
+"want to buy" + card.amount + " " + card.product + " for\n" +
+" a total of" + (card.amount * card.unitprice) + " bucks?";
