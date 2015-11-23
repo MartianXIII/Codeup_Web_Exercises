814576688379f339437d2930fe4724b60d3caf80
Mars <marianomarquis@gmail.com>
2015-11-23 14:24:38 -0600

    Template Literals

--- public/ACME6.js
+++ public/ACME6.js
@@ -171,4 +171,9 @@ var chars = str.split(""); // ["f", "o", "o"]
 //TEMPLATE LITERALS --- STRING INTERPOLATION
 //ACME6 originally known as Template Strings
 var customer = { name: "Foo"}
-var card = { amount:7, product: "Bar", unitprice: 42}
+var card = { amount: 7, product: "Bar", unitprice: 42 }
+message = `Hello ${customer.name},
+want to buy ${card.amount} ${card.product} for
+a total of ${card.amount * card.unitprice} bucks?`
+
+//ACME5
