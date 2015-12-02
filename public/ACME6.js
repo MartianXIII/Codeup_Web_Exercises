cbaf2a76e951f9e6069cf1bd2e37e5c24b99aa53
Mars <marianomarquis@gmail.com>
2015-12-01 19:13:11 -0600

    acme6js

--- public/ACME6.js
+++ public/ACME6.js
@@ -182,3 +182,6 @@ var card = { amount: 7, product: "Bar", unitprice: 42 };
 message = "Hello" + customer.name + ",\n" +
 "want to buy" + card.amount + " " + card.product + " for\n" +
 " a total of" + (card.amount * card.unitprice) + " bucks?";
+
+//Custom Interpolation ACME6
+get`http://example.com/foo?bar=${bar + baz}&quux=${quux}`
